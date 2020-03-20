4edad3e3a742440fa3dd3169ddfde4932381da9e
IsseiMori <ganntyu1225issei@gmail.com>
2020-03-20 14:28:40 -0700

    Updates

---
+++ precache-manifest.526956ba548d396cc6f8c8089498362a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585325808ab8553edc14d229d7da1a06",
+    "url": "/ReactAiConnectFour/index.html"
+  },
+  {
+    "revision": "3f6c264147826fde0b5e",
+    "url": "/ReactAiConnectFour/static/css/main.f807df67.chunk.css"
+  },
+  {
+    "revision": "567cb62253307f2611e1",
+    "url": "/ReactAiConnectFour/static/js/2.cdb4fe8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAiConnectFour/static/js/2.cdb4fe8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6c264147826fde0b5e",
+    "url": "/ReactAiConnectFour/static/js/main.a5e60ac6.chunk.js"
+  },
+  {
+    "revision": "7d734b9d82fc27eeba78",
+    "url": "/ReactAiConnectFour/static/js/runtime-main.f450c979.js"
+  }
+]);
